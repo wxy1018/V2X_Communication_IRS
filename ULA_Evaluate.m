9b259bf9db8ff27523f8ba263fdb23e3ffb7082b
barbaalba <mehdi.haghshenas93@yahoo.com>
2021-10-20 10:44:24 +0200

    Update ULA_Evaluate.m

--- ULA_Evaluate.m
+++ ULA_Evaluate.m
@@ -1,19 +1,19 @@
-function ULA_response = ULA_Evaluate(lambda,N,Angle)
+function ULA_response = ULA_Evaluate(lambda,N,Azimuth)
 % To evaluate normalized antenna vector for Uniform Linear Array 
 % Inputs: 
 %   lambda: Wavelength
 %   N: Number of antenna array 
-%   Angle: Azimuth of Arrival/Departure
+%   Azimuth: Azimuth of Arrival/Departure
 % 
 % Output:
 %   ULA_response: Antenna response
 
 d = lambda/2; % antenna spacing 
 
-ULA_response = zeros(N,length(Angle));
+ULA_response = zeros(N,length(Azimuth));
 
-for n = 1:length(Angle)
+for n = 1:length(Azimuth)
 
-    ULA_response(:,n) = 1 / sqrt(N) * exp(1i*2*pi*d/lambda*sin(Angle(n))*(0:N-1)');
+    ULA_response(:,n) = 1 / sqrt(N) * exp(1i*2*pi*d/lambda*sin(Azimuth(n))*(0:N-1)');
 
 end
