5e2dae2f3ea1a1bf2107b88d025d93eb3beb4345
barbaalba <mehdi.haghshenas93@yahoo.com>
2021-10-20 10:14:28 +0200

    Update ULA_Evaluate.m
    to make it cleaner

--- ULA_Evaluate.m
+++ ULA_Evaluate.m
@@ -1,10 +1,10 @@
-function ULA_response = ULA_Evaluate(lambda,N,AOD)
+function ULA_response = ULA_Evaluate(lambda,N,Angle)
 d = lambda/2; % antenna spacing 
 
-ULA_response = zeros(N,length(AOD));
+ULA_response = zeros(N,length(Angle));
 
-for n = 1:length(AOD)
+for n = 1:length(Angle)
 
-    ULA_response(:,n) = 1 / sqrt(N) * exp(1i*2*pi*d/lambda*sin(AOD(n))*(0:N-1)');
+    ULA_response(:,n) = 1 / sqrt(N) * exp(1i*2*pi*d/lambda*sin(Angle(n))*(0:N-1)');
 
 end
