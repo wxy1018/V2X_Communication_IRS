8b41920c2503a58ad166a40301f61c177585d6c5
barbaalba <mehdi.haghshenas93@yahoo.com>
2021-11-09 13:59:47 +0100

    Update channelEval.m
    Added support for multiple user dynamic scenario

--- channelEval.m
+++ channelEval.m
@@ -1,5 +1,5 @@
-function [h1,h2, h3, h4] = channelEval(t_instance,cond,NamingStruc)
-[h1, h2, h3, h4 ] = deal(0);
+function ChannelMatrix = channelEval(t_instance,cond,NamingStruc,ChannelStruct)
+% [h1, h2, h3, h4 ] = deal(0);
 if strcmp(cond, 'RIS_UE_Channel')
     
     M_H_Rx= 1; % Number of antennas in horizontal axis @ Rx
@@ -14,19 +14,22 @@ if strcmp(cond, 'RIS_UE_Channel')
         RIS = eval(['t_instance.RIS_' num2str(R_idx)]); % Extract soecific RIS
         fn = fieldnames(RIS); % Extract the name of all fields
     
-        Rx_channel = zeros(numel(fn)-1,M_H_Tx*M_V_Tx); % Channel for each RIS
+        Rx_channel = zeros(numel(fieldnames(NamingStruc)),M_H_Tx*M_V_Tx); % Channel for each RIS
     
         %Loop over vehicles (RX)
         for Rx_idx = 1:numel(fn)-1
         
             Rx = RIS.(fn{Rx_idx+1});
-            Rx_channel(Rx_idx,:) = ...
-                RIS_ChannelEval(Rx,M_H_Tx,M_V_Tx,M_H_Rx,M_V_Rx); % [ 1 * N_Tx]
+            Rx_channel(NamingStruc.(fn{Rx_idx+1}),:) = ...
+                RIS_ChannelEval(Rx,M_H_Tx,M_V_Tx,M_H_Rx,M_V_Rx); % [ 1 * N_RIS]
  
         end
     
-        eval(['h' num2str(R_idx) '=Rx_channel']);
+        eval(['ChannelStruct.RIS' num2str(R_idx) ' = Rx_channel']);
+
     end
+    
+    ChannelMatrix = ChannelStruct;
     
 elseif strcmp(cond, 'RIS_BS_Channel')
     
@@ -35,16 +38,21 @@ elseif strcmp(cond, 'RIS_BS_Channel')
     
     M_H_Rx = 16; % Number of antennas in horizontal axis @ Rx
     M_V_Rx = 16; % Number of antennas in vertical axis @ Rx
-   
-    % Loop over all RIS deployed in the cell
-    for R_idx = 1 : t_instance.RIS_number
+    
+    for BS_idx = 1:t_instance.BS_number
         
-        Rx = eval(['t_instance.BS_1.RIS_' ...
-            num2str(R_idx)]); % extract soecific RIS
-        Rx_channel = ...
-            RIS_ChannelEval(Rx,M_H_Tx,M_V_Tx,M_H_Rx,M_V_Rx); % [N_RX * N_TX]
-        eval(['h' num2str(R_idx) '=Rx_channel']);
-    end
+        % Loop over all RIS deployed in the cell
+        for R_idx = 1 : t_instance.RIS_number
+        
+            Rx = eval(['t_instance.BS_' num2str(BS_idx) '.RIS_' ...
+                num2str(R_idx)]); % extract soecific RIS
+            Rx_channel = ...
+                RIS_ChannelEval(Rx,M_H_Tx,M_V_Tx,M_H_Rx,M_V_Rx); % [N_RX * N_TX]
+            eval(['ChannelStruct.BS' num2str(BS_idx), '.RIS' num2str(R_idx) ' = Rx_channel;']);
+        end
+    end 
+    
+    ChannelMatrix = ChannelStruct;
     
 elseif strcmp(cond, 'BS_UE_Channel')
     
@@ -54,22 +62,21 @@ elseif strcmp(cond, 'BS_UE_Channel')
     M_H_Rx = 1; % Number of antennas in horizontal axis @ Rx
     M_V_Rx = 1; % Number of antennas in vertical axis @ Rx
     
-    extData = 5;
+    for BS_idx = 1:t_instance.BS_number 
         
-    fn = fieldnames(t_instance.BS_1); % Extract the name of all fields
-    Rx_channel = zeros(numel(fn) - extData,M_H_Tx*M_V_Tx); % Channel for each RIS
-    
+        eval(['fn = fieldnames(t_instance.BS_' num2str(BS_idx) ');' ]); % Extract the name of all fields
 
-    for Rx_idx = 1 : numel(fn) - extData
+        for Rx_idx = t_instance.RIS_number+2 : numel(fn) 
         
-        Rx = t_instance.BS_1.(fn{Rx_idx + extData});
-        Rx_channel(Rx_idx,:) = ...
-                RIS_ChannelEval(Rx,M_H_Tx,M_V_Tx,M_H_Rx,M_V_Rx); % [ 1 * N_Tx]
+            eval( ['Rx = t_instance.BS_' num2str(BS_idx) '.(fn{Rx_idx});'] );
+            eval(['ChannelStruct.BS' num2str(BS_idx) ...
+                '(NamingStruc.(fn{Rx_idx}),:) = RIS_ChannelEval(Rx,M_H_Tx,M_V_Tx,M_H_Rx,M_V_Rx)']); % [ 1 * N_Tx]
             
+        end
+        
     end
-    
-    h1 = Rx_channel;
-    
+
+    ChannelMatrix = ChannelStruct;
 else
     
 end
